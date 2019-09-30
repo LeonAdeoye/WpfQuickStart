f97b574b2a440cc68a620d0b069e3d32aeed0116
LeonAdeoye <leon.adeoye@gmail.com>
2019-10-01 00:28:40 +0800

    Created assemblies

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfQuickStart.Properties
+namespace InterfacesModuleLibrary.Properties
 {
 
 
