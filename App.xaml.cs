9ad26ccbeb800183926f3ed8103c64c4a2abc956
LeonAdeoye <leon.adeoye@gmail.com>
2019-10-03 23:39:22 +0800

    added config

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,12 @@ namespace WpfQuickStart
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // The bootstrapper will create the Shell so no need to set StartupUri in XAML
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
     }
 }
